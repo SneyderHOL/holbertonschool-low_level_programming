da04715c7f6f221497714d9dba3d8695b922c93d
SneyderHOL <1507@holbertonschool.com>
2020-02-06 17:37:59 +0000

    Adding answer, task 4:6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
