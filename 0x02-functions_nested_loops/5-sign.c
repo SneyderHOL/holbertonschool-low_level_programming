72bea33caa21821327f03c55905f02dbc609ddac
SneyderHOL <1507@holbertonschool.com>
2020-02-11 04:15:48 +0000

    Adding answer, task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *print_sign - prints the sign of a number
+ *
+ *Return: 1 if character is grater than zero, -1 is less than zero, 0 if zero
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar(43);
+		return 1;
+	}
+	else if(c < 0)
+	{
+		_putchar(45);
+		return -1;
+	}
+	else
+	{
+		_putchar(48);
+		return 0;
+	}
+}
