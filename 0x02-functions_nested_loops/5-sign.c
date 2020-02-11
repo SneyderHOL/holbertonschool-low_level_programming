7c430d13aeb02cf61f1bc5feda81e12d1d5c1e6f
SneyderHOL <1507@holbertonschool.com>
2020-02-11 14:35:28 +0000

    Fixing task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  *print_sign - prints the sign of a number
- *
+ *@c: value of character
  *Return: 1 if character is grater than zero, -1 is less than zero, 0 if zero
  */
 int print_sign(int c)
@@ -9,16 +9,16 @@ int print_sign(int c)
 	if (c > 0)
 	{
 		_putchar(43);
-		return 1;
+		return (1);
 	}
 	else if (c < 0)
 	{
 		_putchar(45);
-		return -1;
+		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		return 0;
+		return (0);
 	}
 }
