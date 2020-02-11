eb22dbbccbb5915403cd11ea59d3a7913889e0db
SneyderHOL <1507@holbertonschool.com>
2020-02-11 14:38:47 +0000

    Fixing parameter name in task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
  *print_sign - prints the sign of a number
- *@c: value of character
+ *@n: value of character
  *Return: 1 if character is grater than zero, -1 is less than zero, 0 if zero
  */
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
