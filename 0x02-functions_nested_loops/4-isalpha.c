a0266bf9852eb5ad7f84acb8488d5a90a586e81f
SneyderHOL <1507@holbertonschool.com>
2020-02-11 04:00:35 +0000

    Adding answer, task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isalpha - code that checks for alphabetic character
+ *
+ *Return: 1 if character is lowercase, uppercase or letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
