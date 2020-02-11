ff1d63b21232ac06f08e0240a7aca29f490fa382
SneyderHOL <1507@holbertonschool.com>
2020-02-11 14:41:24 +0000

    Fixing task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  *_abs - compute the absolute value of an integer
- *
+ *@c: int value to operate
  *Return: return the absolute value
  */
 int _abs(int c)
 {
 	if (c < 0)
 	{
-		return c * (-1);
+		return (c * (-1));
 	}
-	return c;
+	return (c);
 }
