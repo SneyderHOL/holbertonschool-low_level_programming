ee6e434b542c6a5da7c4db888b822898c999e646
SneyderHOL <1507@holbertonschool.com>
2020-02-11 03:00:48 +0000

    Adding answer, task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *pritn_alphabet - code for printing the alphabet
+ *
+ *Return: no return value
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	int x;
+
+	for(i = 0; i <10; i++)
+	{
+		for(x = 97; x <123; x++)
+		{
+			_putchar(x);
+		}
+		_putchar(10);
+	}
+}
