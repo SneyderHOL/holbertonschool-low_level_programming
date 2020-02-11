23e908154d936d911ae6b98269a4a5a5e4aeffc6
SneyderHOL <1507@holbertonschool.com>
2020-02-11 14:30:56 +0000

    Fixing task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  *_islower - code that checks for lowercase character
- *
+ *@c: value of the character
  *Return: 1 if character is lowercase, 0 otherwise
  */
 int _islower(int c)
