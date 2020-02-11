07922f098e62eeeaddc12df14eb4bf58b043dc76
SneyderHOL <1507@holbertonschool.com>
2020-02-11 04:38:25 +0000

    Adding answer, task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_abs - compute the absolute value of an integer
+ *
+ *Return: return the absolute value
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return c * (-1);
+	}
+	return c;
+}
