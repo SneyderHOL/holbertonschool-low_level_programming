da931cebb4c70f6e6d629896570d6bb36262d00c
SneyderHOL <1507@holbertonschool.com>
2020-02-11 18:29:14 +0000

    Adding answer, task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_to_98 - adds two integers
+ *@n: is an integer
+ *
+ *
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n < 98; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	}
+	printf("%d\n", n);
+}
