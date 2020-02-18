4e3b370b9bce61a00aa0c2e64dcec7ded470f29c
SneyderHOL <1507@holbertonschool.com>
2020-02-18 22:37:01 +0000

    Fixing answer task 10, betty style

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-typedef enum {false, true} bool;
+
 /**
  *_atoi - convert a string to an integer
  *
@@ -8,9 +8,7 @@ typedef enum {false, true} bool;
  */
 int _atoi(char *s)
 {
-	int a, aux = 1, n = 0;
-
-	bool num = false;
+	int a, aux = 1, n = 0, num = 0;
 
 	for (a = 0; *(s + a) != 0; a++)
 	{
@@ -20,12 +18,12 @@ int _atoi(char *s)
 			aux *= 1;
 		if (*(s + a) > 47 && *(s + a) < 58)
 		{
-			num = true;
-			if (n > 0 && num)
+			num = 1;
+			if (n > 0 && num == 1)
 				n *= 10;
 			n += *(s + a) - '0';
 		}
-		else if (n > 0 && num)
+		else if (n > 0 && num == 1)
 			break;
 	}
 	return (aux * n);
