728ec604108504e30cfd2ab3461616bce6700cd0
SneyderHOL <1507@holbertonschool.com>
2020-02-18 02:23:49 +0000

    Adding _putchar function, holberton header and answer for task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - takes a pointer to an int as parameter and updates the value it
+ *points to 98
+ *@n: int pointer to work
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
