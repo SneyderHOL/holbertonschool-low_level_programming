e942af6a44408226fb2f39fe65f54e8a1fd318a4
SneyderHOL <1507@holbertonschool.com>
2020-02-20 19:50:14 +0000

    Fixing task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,24 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a, b;
-
+	/*int a, b;
+	 */
+	int a;
 	for (a = 0; *(s1 + a) != 0; a++)
 	{
-	}
+		if (s1[a] < s2[a])
+			return (s1[a] - s2[a]);
+		if (s1[a] > s2[a])
+			return (s1[a] - s2[a]);
+	}/*
 	for (b = 0; *(s2 + b) != 0; b++)
 	{
 	}
-	if (s1[0] < s2[0])
-		return (s1[0] - s2[0]);
-	if (s1[0] > s2[0])
-		return (s1[0] - s2[0]);
+	
+
+	if(a < b)
+		return (a-b);
+	if(b)
+	 */
 	return (0);
 }
