a78ddf6fba8eee696636aba8a5bbbacd25f601f6
SneyderHOL <1507@holbertonschool.com>
2020-02-20 22:56:12 +0000

    Ejercicio 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * leet - encodes a string into 1337.
+ *@n: char pointer
+ *
+ *
+ *Return: char pointer
+ */
+char *leet(char *n)
+{
+	/*int mat[] = {{97, 65, 52}, {101, 69, 51}, {111, 69, 51}, {116, 84, 55}, {108, 76, 49}};*/
+
+	int A[] = {97, 65, 52}, E[] = {101, 69, 51}, O[] = {111, 69, 51}, T[] = {116, 84, 55}, L[] = {108, 76, 49};
+
+	int a, b, e = 2;
+	for (a = 0; *(n + a); a++)
+	{
+		for (b = 0; b < e; b++)
+		{
+			if(*(n + a) == )
+			{
+
+			}
+		}
+	}
+	return (n);
+}
