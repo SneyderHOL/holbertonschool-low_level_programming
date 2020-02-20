58044fd1e7054775e7910ce8cea0316b37d93092
SneyderHOL <1507@holbertonschool.com>
2020-02-20 19:52:42 +0000

    Fixing task 3, betty style

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	/*int a, b;
-	 */
 	int a;
+
 	for (a = 0; *(s1 + a) != 0; a++)
 	{
 		if (s1[a] < s2[a])
 			return (s1[a] - s2[a]);
 		if (s1[a] > s2[a])
 			return (s1[a] - s2[a]);
-	}/*
-	for (b = 0; *(s2 + b) != 0; b++)
-	{
 	}
-	
-
-	if(a < b)
-		return (a-b);
-	if(b)
-	 */
 	return (0);
 }
