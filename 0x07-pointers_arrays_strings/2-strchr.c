8f8bcd922e588b503746695947622f27008a538a
SneyderHOL <1507@holbertonschool.com>
2020-02-24 17:54:23 +0000

    Fixing task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,16 @@ char *_strchr(char *s, char c)
 {
 	unsigned int a;
 
+	char aux = *s; 
 	for (a = 0; *(s + a) != 0; a++)
 	{
 		if (c == *(s + a))
 		{
+			s = (s + a);
 			break;
 		}
 	}
-	return (s + a);
+	if (aux == *s)
+		s = NULL;
+	return (s);
 }
