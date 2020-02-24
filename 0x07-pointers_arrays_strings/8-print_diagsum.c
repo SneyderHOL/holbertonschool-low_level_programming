f442f8c3a87ff01ba03b32514e07cb8c6884a881
SneyderHOL <1507@holbertonschool.com>
2020-02-24 23:43:41 +0000

    Adding task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - print the sum of the two diagonals of a square matrix of
+ *integers.
+ *@a: int pointer
+ *@size: integer
+ */
+void print_diagsums(int *a, int size)
+{
+	int x, b, c, aux1 = 0, aux2 = 0;
+
+	for (x = 0; x < size; x++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				if (b = 3 / 2)
+					aux1 = aux1 + a[x][b][c];
+				if()
+			}
+			aux = aux + 
+		}
+		_putchar('\n');
+	}
+}
