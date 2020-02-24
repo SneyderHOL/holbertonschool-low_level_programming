0c820badfb10a18de58b513f764cbf53ab7d36c9
SneyderHOL <1507@holbertonschool.com>
2020-02-24 19:44:22 +0000

    Adding task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *_strpbrk - searches a string for any of a set of bytes.
+ *@s: char pointer
+ *@accept: char pointer
+ *
+ *Return: char pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int aux = 0, a = 0, b;
+
+	for (b = 0; *(s + b) != 0; b++)
+	{
+		for (a = 0; *(accept + a) != 0; a++)
+		{
+			if (*(s + b) == *(accept + a))
+			{
+				aux++;
+				break;
+			}
+		}
+		if (aux != 0)
+			break;
+	}
+	return (s + aux);
+}
