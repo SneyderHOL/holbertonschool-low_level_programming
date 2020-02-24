b7f173e258356673c7479197e666be2f792b14bf
SneyderHOL <1507@holbertonschool.com>
2020-02-24 22:19:02 +0000

    Fixing task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,17 +9,12 @@ void print_chessboard(char (*a)[8])
 {
 	int b, i = 0;
 
-	char c;
-
 	for (b = 0; b < 8; b++)
 	{
 		for (i = 0; i < 8; i++)
 		{
-			c = a[b][i];
-			if (c != ' ')
-				_putchar(c);
-			if (i == 7)
-				_putchar('\n');
+			_putchar(a[b][i]);
 		}
+		_putchar('\n');
 	}
 }
