867473a5e2204373fa5f91a56ef84055c7235ec2
SneyderHOL <1507@holbertonschool.com>
2020-02-29 04:03:54 +0000

    Fixing task 4 and adding task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *@argc: integer
+ *@argv: an array of pointers
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int aux = 0, arreglo[] = {25, 10, 5, 2, 1}, a = 0, val = 0, *ar = NULL;
+
+	ar = arreglo;
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	aux = atoi(argv[1]);
+	if (aux < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		while (aux != val)
+		{
+			if (aux >= (val + *ar))
+			{
+				val += *ar;
+				a++;
+			}
+			else
+			{
+				ar++;
+			}
+		}
+		printf("%d\n", a);
+	}
+	return (0);
+}
