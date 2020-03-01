27d1ef92720c1dbe2c615451bb812cde77485e51
SneyderHOL <1507@holbertonschool.com>
2020-03-01 13:52:58 +0000

    Fix task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,24 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int aux = 0, a = 0, val = 0, b = 0, cent[] = {25, 10, 5, 2, 1}, *ar = NULL;
+	int aux = 0, a = 0, val = 0, cent[] = {25, 10, 5, 2, 1}, *ar = NULL;
 
 	ar = cent;
 	if (argc == 2)
 	{
 		aux = atoi(argv[1]);
-		if (aux < 0)
+		if (aux <= 0)
 		{
 			printf("0\n");
 			return (0);
-		}
-		for (; argv[1][b]; b++)
-		{
-			if (argv[1][b] > 57 || argv[1][b] < 48)
-			{
-				printf("Error\n");
-				return (1);
-			}
 		}
 		while (aux != val)
 		{
@@ -43,7 +35,7 @@ int main(int argc, char *argv[])
 	else
 	{
 		printf("Error\n");
-		return (0);
+		return (1);
 	}
 	return (0);
 }
