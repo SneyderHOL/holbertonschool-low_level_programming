cd00a077bd9ab179cade0191b015c6389c312432
SneyderHOL <1507@holbertonschool.com>
2020-03-01 04:35:07 +0000

    Fix task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  *main - Entry point
  *@argc: integer
@@ -12,35 +12,38 @@ int main(int argc, char *argv[])
 	int aux = 0, a = 0, val = 0, b = 0, cent[] = {25, 10, 5, 2, 1}, *ar = NULL;
 
 	ar = cent;
-	if (argc <= 1 || argc > 2)
+	if (argc == 2)
+	{
+		aux = atoi(argv[1]);
+		if (aux < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+		for (; argv[1][b]; b++)
+		{
+			if (argv[1][b] > 57 || argv[1][b] < 48)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		while (aux != val)
+		{
+			if (aux >= (val + *ar))
+			{
+				val += *ar;
+				a++;
+			}
+			else
+				ar++;
+		}
+		printf("%d\n", a);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	aux = atoi(argv[1]);
-	if (aux < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (; argv[1][b]; b++)
-	{
-		if (argv[1][b] > 57 || argv[1][b] < 48)
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-	while (aux != val)
-	{
-		if (aux >= (val + *ar))
-		{
-			val += *ar;
-			a++;
-		}
-		else
-			ar++;
-	}
-	printf("%d\n", a);
 	return (0);
 }
