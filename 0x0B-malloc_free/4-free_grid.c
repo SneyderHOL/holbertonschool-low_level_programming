2b52e945af5e6ffda36e66be257825cad541b636
SneyderHOL <1507@holbertonschool.com>
2020-03-04 06:58:04 +0000

    Fix task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  *free_grid - frees a 2 dimensional grid previously created by alloc_grid
  *
@@ -9,24 +8,14 @@
  */
 void free_grid(int **grid, int height)
 {
-	int **mat = NULL, a = 0, b = 0;
+	int a = 0;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-	mat = malloc(height * sizeof(int *));
-	if (mat == NULL)
-		return (NULL);
+	if (height <= 0)
+		return;
+	if (!grid)
+		return;
 	for (a = 0; a < height; a++)
 	{
-		mat[a] = (int *) malloc(width * sizeof(int));
-		if (mat[a] == NULL)
-			return (NULL);
-		for (b = 0; b < width; b++)
-		{
-			mat[a][b] = 0;
-		}
-
+		free(grid + a);
 	}
-
-	return (mat);
 }
