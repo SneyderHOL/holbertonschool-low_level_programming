e36c4084a80f89a596348c610797bc069fa1f530
SneyderHOL <1507@holbertonschool.com>
2020-03-04 07:34:49 +0000

    Fix task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,15 +8,17 @@
  */
 void free_grid(int **grid, int height)
 {
-	int a = 0, b = 0;
+	int a = 0, b = 0, size = 0;
 
 	if (height <= 0)
 		return;
 	if (!grid)
 		return;
+	size = sizeof(*(grid + 0)) / ((grid + 1) - (grid + 0));
+	size = size / sizeof(**grid);
 	for (a = 0; a < height; a++)
 	{
-		for (b = 0; b < 6; b++)
+		for (b = 0; b < size; b++)
 			free(grid[a] + b);
 		free(grid + a);
 	}
