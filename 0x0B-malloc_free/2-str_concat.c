bbdca0157d2fda9412af2f0834adc76f0201e478
SneyderHOL <1507@holbertonschool.com>
2020-03-04 00:13:08 +0000

    Fix task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,30 +31,28 @@ char *str_concat(char *s1, char *s2)
 
 	if (!*s1 && !*s2)
 		return (NULL);
-	if (*s1)
-		for (i = 0; *(s1 + i); i++)
-		{
-		}
-	if (*s2)
-		for (j = 0; *(s2 + j); j++)
-		{
-		}
+	for (i = 0; *(s1 + i); i++)
+	{
+	}
+	for (j = 0; *(s2 + j); j++)
+	{
+	}
+	poin = (char *) malloc((i + j + 1) * sizeof(char));
+	if (poin == NULL)
+		return (NULL);
 	if (i != 0 && j != 0)
 	{
-		poin = (char *) malloc((i + j + 1) * sizeof(char));
 		a = concat(0, i, poin, s1);
 		a = concat(i, j, poin, s2);
 		*(poin + a) = *(s2 + a - i);
 	}
 	else if (i != 0)
 	{
-		poin = (char *) malloc((i + 1) * sizeof(char));
 		a = concat(0, i, poin, s1);
 		*(poin + a) = *(s1 + a);
 	}
 	else
 	{
-		poin = (char *) malloc((j + 1) * sizeof(char));
 		a = concat(0, j, poin, s2);
 		*(poin + a) = *(s2 + a);
 	}
