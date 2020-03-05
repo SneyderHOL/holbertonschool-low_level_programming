647516c7babc5bc0f0c9fb6503c652491f35a56e
SneyderHOL <1507@holbertonschool.com>
2020-03-05 23:51:20 +0000

    Fix holberton.h and task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,20 @@
 #include "holberton.h"
 /**
+ *copyp - copy the content of a pointer to another
+ *@s1: char pointer argument
+ *@s2: char pointer argument
+ *@n: unsigned int argument
+ *
+ */
+void copyp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+		*(s1 + i) = *(s2 + i);
+}
+
+/**
  *_realloc - reallocates a memory block using malloc and free
  *@ptr: void pointer
  *@old_size: unsigned int
@@ -8,7 +23,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p = NULL;
+	char *p = NULL;
+	unsigned int a = 0;
 
 	if (new_size == 0 && !ptr)
 	{
@@ -19,7 +35,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 	if (new_size == old_size)
 		return (ptr);
+	if (new_size < old_size)
+	{
+		a = new_size;
+		p = ptr;
+		*(p + a) = '\0';
+		for (a++; a < old_size + 1; a++)
+		{
+			free(p + a);
+		}
+		return (ptr);
+	}
 	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	copyp(p, ptr, old_size);
 	free(ptr);
 	return (p);
 }
