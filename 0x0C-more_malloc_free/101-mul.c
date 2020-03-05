0ecbc3bde72f42974d5a9ed3061e7e9f48c7fa67
SneyderHOL <1507@holbertonschool.com>
2020-03-05 20:17:49 +0000

    Fix task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <ctype.h>
 /**
  *main - multiplies two positive numbers
  *@argc: int argument
@@ -13,6 +14,22 @@ int main(int argc, char **argv)
 	{
 		printf("Error\n");
 		exit(98);
+	}
+	for (a = 0; argv[1][a]; a++)
+	{
+		if (!isdigit(argv[1][a]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	for (a = 0; argv[2][a]; a++)
+	{
+		if (!isdigit(argv[2][a]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
 	}
 	a = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", a);
