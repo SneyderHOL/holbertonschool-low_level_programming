826e103fa55c66c2027da4fb567057a50a17bfa6
SneyderHOL <1507@holbertonschool.com>
2020-03-05 20:30:13 +0000

    Fix task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,6 +8,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *p = NULL;
+
 	if (new_size == 0 && !ptr)
 	{
 		free(ptr);
@@ -17,6 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 	if (new_size == old_size)
 		return (ptr);
-	ptr = malloc(new_size);
-	return (ptr);
+	p = malloc(new_size);
+	free(ptr);
+	return (p);
 }
