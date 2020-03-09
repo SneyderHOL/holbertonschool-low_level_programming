9adcb2da58de379e77ca62c82f6eecc143dd3671
SneyderHOL <1507@holbertonschool.com>
2020-03-09 21:06:42 +0000

    Fix task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *_strdup - returns a pointer to a newly allocated space in memory, which
+ *contains a copy of the string given as a parameter
+ *@str: char pointer argument
+ *
+ *Return: char pointer
+ */
+char *_strdup(char *str)
+{
+	char *poin = NULL;
+
+	int i, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; *(str + i); i++)
+	{
+		j++;
+	}
+	j++;
+	poin = malloc(j * sizeof(char));
+	if (poin == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		*(poin + i) = *(str + i);
+	return (poin);
+}
+/**
  *new_dog - creates a new dog
  *@name: char pointer argument
  *@age: float argument
@@ -10,16 +37,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *nombre = "", *dueno = "";
+	char *nombre = NULL, *dueno = NULL;
 	dog_t *dog;
 
 	if (name != NULL)
-		nombre = name;
+	{
+		nombre = _strdup(name);
+		if (nombre == NULL)
+			return (NULL);
+	}
 	if (owner != NULL)
-		dueno = owner;
+	{
+		dueno = _strdup(owner);
+		if (dueno == NULL)
+		{
+			free(nombre);
+			return (NULL);
+		}
+	}
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
+		free(nombre);
+		free(dueno);
 		return (NULL);
+	}
 	dog->name = nombre;
 	dog->age = age;
 	dog->owner = dueno;
