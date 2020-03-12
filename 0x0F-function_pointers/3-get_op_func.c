a80753a347e9cb4caa4f1c541474d3bf85556b32
SneyderHOL <1507@holbertonschool.com>
2020-03-12 03:42:47 +0000

    Fix task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *get_op_func - selects the operation to perform
  *@s: char pointer argument
@@ -9,8 +9,6 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
-
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -19,6 +17,8 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i = 0;
+
 	for (i = 0; i < 5; i++)
 	{
 		if (ops[i].op[0] == s[0])
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,6 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 /**
  *main - Entry point
  *@argc: integer argument
