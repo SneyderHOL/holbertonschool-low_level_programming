a0d1e581a1f9d8ed71c7ffd225ee52e512e35b3b
SneyderHOL <1507@holbertonschool.com>
2020-03-12 03:49:17 +0000

    Fix task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *get_op_func - selects the operation to perform
+ *@s: char pointer argument
+ *
+ *Return: A pointer to the function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+/**
+ *op_add - function that adds two numbers
+ *@a: integer argument
+ *@b: integer argument
+ *Return: a + b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ *op_sub - function that substract two numbers
+ *@a: integer argument
+ *@b: integer argument
+ *Return: a - b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ *op_mul - function that multiply two numbers
+ *@a: integer argument
+ *@b: integer argument
+ *Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ *op_div - function that divide two numbers
+ *@a: integer argument
+ *@b: integer argument
+ *Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ *op_mod - function that gets the module of a number
+ *@a: integer argument
+ *@b: integer argument
+ *Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
