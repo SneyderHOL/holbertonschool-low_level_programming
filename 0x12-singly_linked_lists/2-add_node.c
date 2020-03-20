6edc0bf80415292a93277a81e028c5daf99dbfa8
SneyderHOL <1507@holbertonschool.com>
2020-03-20 01:33:41 +0000

    Fix task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,20 +5,21 @@
  * list_t list.
  * @head: pointer to a list_t pointer argument
  * @str: char pointer argument
- * Return: the number of nodes
+ * Return: the head of the list
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	unsigned int cont = 0;
 	list_t *node = NULL;
 
+	if (head == NULL)
+		return (NULL);
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
-	node->str = strdup(str);
-	node->next = NULL;
 	while ((node->str)[cont] != '\0')
 		cont++;
+	node->str = strdup(str);
 	node->len = cont;
 	node->next = *head;
 	*head = node;
