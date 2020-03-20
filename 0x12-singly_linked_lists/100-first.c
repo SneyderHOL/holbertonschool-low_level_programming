c2b0545bf441a3bc4ce71c9567562bbbbcafd706
SneyderHOL <1507@holbertonschool.com>
2020-03-20 03:42:01 +0000

    Add task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ *thisbeforemain - function that prints You're beat! and yet, you must allow,
+ *\nI bore my house upon my back!\n before the main function is executed.
+ *
+ *Return: 0
+ */
+void __attribute__((constructor)) thisbeforemain(void);
+void thisbeforemain(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
