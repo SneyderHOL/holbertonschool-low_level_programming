179175c8481d4fb15707a86c67670c4eb88a07bf
SneyderHOL <1507@holbertonschool.com>
2020-03-24 14:48:13 +0000

    Fix task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,7 +12,7 @@ int pop_listint(listint_t **head)
 	listint_t *aux = *head;
 	int tmp_n = 0;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	tmp_n = aux->n;
 	*head = aux->next;
