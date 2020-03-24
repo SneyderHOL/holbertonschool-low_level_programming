e5fcf0fe794bebeb5de45b59137061afeb14b96b
SneyderHOL <1507@holbertonschool.com>
2020-03-24 23:32:40 +0000

    Fix task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,7 +12,7 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *turtle = head;
 
 	if (head == NULL)
-		exit(98);
+		return(0);
 	while (turtle != NULL && turtle > turtle->next)
 	{
 		printf("[%p] %i\n", (void *) turtle, turtle->n);
