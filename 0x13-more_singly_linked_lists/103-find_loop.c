20ad4052cbd8654ac7079e133072be81a1d1d578
SneyderHOL <1507@holbertonschool.com>
2020-03-25 19:47:01 +0000

    Fix task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,7 +11,7 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *hare = head;
 	listint_t *turtle = head;
 
-	if (head == NULL || head->next != NULL)
+	if (head == NULL || head->next == NULL)
 		return (NULL);
 	turtle = turtle->next;
 	hare = hare->next->next;
@@ -19,7 +19,7 @@ listint_t *find_listint_loop(listint_t *head)
 	{
 		if (turtle == hare)
 		{
-			turtle = head;
+			hare = head;
 			while (turtle != hare)
 			{
 				turtle = turtle->next;
