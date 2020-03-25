af45b952ffe8009e2d29521cdbaf263319557e3c
SneyderHOL <1507@holbertonschool.com>
2020-03-25 01:55:06 +0000

    Add task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_listint_safe - function that free a listint_t list.
+ * free_listint_safe - function that frees a listint_t list.
  * @head: pointer to the head of a listint element
- * Return: a pointer to the first node of the reversed list
+ * Return: the size of the list that was free'd
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
+	unsigned int num = 0;
 	listint_t *aux = *head;
-	listint_t *tmp = *head;
 
-	if (*head == NULL)
-		return (NULL);
-	while (aux->next != NULL)
+	if (head == NULL || *head == NULL)
+		return (0);
+	while (aux != NULL)
 	{
-		if (tmp != *head)
-			tmp = *head;
-		*head = aux->next;
-		aux->next = (*head)->next;
-		(*head)->next = tmp;
+		free(aux);
+		num++;
+		if (aux <= aux->next)
+			break;
+		aux = aux->next;
 	}
-	return (*head);
+	*head = NULL;
+	return (num);
 }
