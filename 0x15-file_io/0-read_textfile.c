7884aebd6629b0ee1a81f86495b8996ab2570569
SneyderHOL <1507@holbertonschool.com>
2020-03-31 19:21:01 +0000

    Fix task 0 checking read function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, aux = 0, res = 0;
 	char *buffer = NULL;
 
-	if (filename == NULL)
+	if (filename == NULL && *filename == '\0')
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -26,7 +26,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(letters * sizeof(char));
 	if (buffer == NULL)
 		return (0);
-	read(fd, buffer, letters);
+	res = read(fd, buffer, letters);
+	if (res == -1)
+		return (0);
 	close(fd);
 	aux = _strlen(buffer);
 	res = write(1, buffer, aux);
