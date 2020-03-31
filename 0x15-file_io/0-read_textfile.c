c7e038dc47465d0f443b88b63522e0ca2e25628b
SneyderHOL <1507@holbertonschool.com>
2020-03-31 19:37:06 +0000

    Fix task 0 checking for stdout in write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, aux = 0, res = 0;
 	char *buffer = NULL;
 
-	if (filename == NULL && *filename == '\0' && letter < 0)
+	if (filename == NULL && *filename == '\0')
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	close(fd);
 	aux = _strlen(buffer);
-	res = write(1, buffer, aux);
+	res = write(STDOUT_FILENO, buffer, aux);
 	free(buffer);
 	if (res != aux)
 		return (0);
