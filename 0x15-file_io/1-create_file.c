816c1527d103892f2cd26babce7ab8cabb58de86
SneyderHOL <1507@holbertonschool.com>
2020-03-31 20:43:48 +0000

    Fix task 1 creation flags

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = creat(filename, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
