59c622484a8129193b93925165edbddf9b596c33
SneyderHOL <1507@holbertonschool.com>
2020-03-31 19:25:18 +0000

    Fix task 0 check for letters

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, aux = 0, res = 0;
 	char *buffer = NULL;
 
-	if (filename == NULL && *filename == '\0')
+	if (filename == NULL && *filename == '\0' && letter < 1)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
