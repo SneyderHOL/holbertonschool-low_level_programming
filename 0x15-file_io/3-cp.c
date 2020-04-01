68beb8d9866f8823a5c4463ede1e25fa833c874c
SneyderHOL <1507@holbertonschool.com>
2020-04-01 01:17:01 +0000

    Fix task 3 remove buffer with malloc

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,8 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-int _strlen(char *s);
-void fill_buffer(char *str, unsigned int size);
+
 /**
  * main - program that copies the content of a file to another file.
  * @argc: argument counter
@@ -16,111 +15,31 @@ void fill_buffer(char *str, unsigned int size);
 int main(int argc, char *argv[])
 {
 	int first_fd, second_fd, aux = 0, len = 0;
-	char *buffer = NULL;
+	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	first_fd = open(argv[1], O_RDONLY);
 	if (first_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]), exit(98);
 	second_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (second_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(first_fd);
-		exit(99);
-	}
-	buffer = malloc(1024);
-	if (buffer == NULL)
-		exit(1);
-	fill_buffer(buffer, 1024);
-	aux = read(first_fd, buffer, 1024);
-	if (aux == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(98);
-	}
-	len = _strlen(buffer);
-	aux = write(second_fd, buffer, len);
-	if (aux == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(first_fd);
-		exit(99);
-	}
-	while (buffer[1023] != '\0')
-	{
-		fill_buffer(buffer, 1024);
-		aux = read(first_fd, buffer, 1024);
-		if (aux == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			close(first_fd);
-			close(second_fd);
-			exit(98);
-		}
-		len = _strlen(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	do {
+		len = read(first_fd, buffer, 1024);
+		if (len == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
 		aux = write(second_fd, buffer, len);
 		if (aux == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(first_fd);
-			exit(99);
-		}
-	}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	} while (len == 1024);
 	aux = close(first_fd);
 	if (aux == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", first_fd);
-		free(buffer);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", first_fd), exit(100);
 	aux = close(second_fd);
 	if (aux == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", second_fd);
-		free(buffer);
-		exit(100);
-	}
-	free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", second_fd), exit(100);
 	return (0);
-}
-
-/**
- *_strlen - calculate the length of a string
- *@s: char pointer
- *
- *Return: the length of a string
- */
-int _strlen(char *s)
-{
-	int a = 0;
-
-	for (a = 0; *(s + a) != 0; a++)
-	{
-	}
-	return (a);
-}
-/**
- * fill_buffer - functions that fills the string with char '\0'
- * @str: string to fill
- * @size: size of the string
- *
- */
-void fill_buffer(char *str, unsigned int size)
-{
-	unsigned int i = 0;
-
-	for (; i < size; i++)
-	{
-		str[i] = '\0';
-	}
 }
